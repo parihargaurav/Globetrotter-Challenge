4f31a49f6501d752e78bc53448b6a9f37d7230d7
parihargaurav <parihargaurav527@gmail.com>
2025-03-07 17:17:26 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: "https://globetrotter-challenge-2-frontend.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
   })
 );
 
