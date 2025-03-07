6f1a668243092f093e7a91d5d4aa27fa97b74a5e
parihargaurav <parihargaurav527@gmail.com>
2025-03-07 17:24:52 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
         <main className="flex-grow flex flex-col">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="#/quiz" element={<Quiz />} />
+            <Route path="/quiz" element={<Quiz />} />
             <Route path="#/invite" element={<Invite />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
